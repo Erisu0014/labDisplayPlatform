85bd97fe566cde3926a64207cb0a00b154542080
Erisu <1269732086@qq.com>
2018-12-25 11:01:28 +0800

    achievement单界面逻辑
    Signed-off-by: Erisu <1269732086@qq.com>

--- src/main/java/org/software/ysu/controller/achievementController.java
+++ src/main/java/org/software/ysu/controller/achievementController.java
@@ -23,15 +23,15 @@ import java.util.List;
 public class achievementController {
     @Resource
     IIntroService introService;
-    @RequestMapping("getAchievements")
+    @RequestMapping("getAchievements.do")
     public tableResponse getSubjects(Page page) {
         System.out.println("page="+page.toString());
          IntroductionExample introductionExample = new IntroductionExample();
         if (page.getContext() != ""){
-            introductionExample.createCriteria().andIntroTitleEqualTo("%" + page.getContext() + "%");
+            introductionExample.createCriteria().andIntroTitleLike("%" + page.getContext() + "%");
         }
         List<IntroductionWithBLOBs>intros=introService.getIntros(introductionExample);
-        int tempMin=Math.min(intros.size(),page.getPage() * page.getLimit()+1);
+        int tempMin=Math.min(intros.size(),page.getPage() * page.getLimit());
         //前台真正显示的数据
         List<IntroductionWithBLOBs>introPages=new ArrayList<>();
         for(int i=(page.getPage() - 1) * page.getLimit();i<tempMin;i++){
@@ -39,5 +39,18 @@ public class achievementController {
         }
         tableResponse tableResponse=new tableResponse("0","",intros.size(),introPages);
         return tableResponse;
+    }
+    @RequestMapping("recentAchievements.do")
+    public List<IntroductionWithBLOBs> getRecents() {
+        IntroductionExample introductionExample = new IntroductionExample();
+        introductionExample.setOrderByClause("'intro_createTime' DESC");
+        List<IntroductionWithBLOBs>intros=introService.getIntros(introductionExample);
+        //前台真正显示的数据
+        List<IntroductionWithBLOBs>introPages=new ArrayList<>();
+        int temp=Math.min(intros.size(),3);
+        for(int i=0;i<temp;i++){
+            introPages.add(intros.get(i));
+        }
+        return introPages;
     }
 }
